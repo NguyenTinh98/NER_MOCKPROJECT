531f6497c507a3634378082a712b50c7903e75f0
Manh Pham <70097274+Phammanh26@users.noreply.github.com>
2021-12-11 00:13:58 +0700

    Update cut256.py

--- utils/cut256.py
+++ utils/cut256.py
@@ -35,3 +35,29 @@ def cutting_subword(X, y, size=256):
     res_X.append(X[cur:])
     res_y.append(y[cur:])
     return res_X, res_y
+
+############
+def isNotSubword(x, idx, sub = '##'):
+    return sub not in x[idx] and idx > 0 and idx < len(x) - 1 and sub not in x[idx-1] and sub not in x[idx+1]
+
+def cutting_subword(X, sub = '##', size=256):
+    res_X = []
+    punct = '.!?'
+    st = 0
+    cur = 0
+    while (st < len(X)-size):
+        flag = True
+        for i in range(st+size-1, st-1, -1):
+            if X[i] in punct:
+                cur = i+1
+                flag = False
+                break
+        if flag:
+            for i in range(st+size-1, st-1, -1):
+                if isNotSubword(X, i, sub):
+                    cur = i+1
+                    break
+        res_X.append(X[st: cur])
+        st = cur
+    res_X.append(X[cur:])
+    return res_X
