67b2771aa7f822982c6e9b5a294d3ed391f65497
Pham vAN mANH <pvm26042000@gmail.com>
2021-12-20 16:52:03 +0700

    update function argument mention method

--- agurment.py
+++ agurment.py
@@ -88,3 +88,108 @@ def  augmentation_SiS(infile, outfile, labels):
     print('done save file!!!!')
     return data_shuffled
             
+
+
+from numpy import random
+
+
+def  augmentation_MR(infile, outfile, labels):
+
+    '''
+    augmentation_MR: phương pháp augmentation data bằng Shuffle within segments, sử dụng phân phối nhị thức để quyết định shuffle cụm label trong câu
+    :infile: path file data gốc (.pkl) 
+    :outfile: path file lưu data đã shuffle (.pkl) 
+    :labels: các loại nhãn sẽ xet shuffle
+
+    : return: list data đã shuffle
+    '''
+
+    new_mentions = []
+
+    print('loading data.....')
+    with open(infile, 'rb') as f:
+        data =pk.load(f)
+    print('complete data')
+
+
+    vocab = get_vocab(data, labels)
+
+    
+    for index in range(len(data)):
+        mentions = []
+
+        segment_sentence = segment_data(data[index], labels)
+        if segment_sentence is not None:
+
+            size = len(segment_sentence)
+            x = random.binomial(n=1, p=0.5, size=size)
+
+            for jndex in range(size):
+                ner = segment_sentence[jndex][1]
+                if x[jndex] == 1 and ner !='O' :
+                    new_word = random.choice(vocab[ner])
+                    mentions.append((new_word, ner))
+                else:
+                    mentions.append(segment_sentence[jndex])
+        
+
+            new_mentions.append(mentions)
+
+
+    
+    print('save file data.......')
+    with open(outfile, 'wb') as f:
+        pk.dump(new_mentions, f)
+    
+    print('done save file!!!!')
+
+    return new_mentions 
+
+
+def get_vocab(data, labels):
+  
+    print('getting vocab....')
+    vocab = {}
+    for dat in data:
+        segment_sent = segment_data(dat, labels)
+        if segment_sent is not None:
+            for word in segment_sent:
+                if word[1] != 'O':
+                    if word[1] in vocab:
+                        vocab[word[1]].append(word[0])
+                    else:
+                
+                        vocab[word[1]] = [word[0]]
+
+    print('Done etting vocab!!!!!')
+    return vocab
+
+
+def segment_data(data, labels):
+    state = 0
+    segment_sentence = []
+    for jndex in range(len(data)):
+        x = data[jndex]
+        if labels == 'all':
+            state = 1
+
+        elif (x[1] in labels) and (state == 0):
+            state = 1
+
+        if jndex == 0:
+            segment_sentence.append(x)
+        else:
+            pre_x = data[jndex - 1]
+            if x[1] == pre_x[1]:
+                pre_sentence = segment_sentence[-1][0]
+                pre_sentence += " " + x[0]
+                ner = x[1]
+
+                segment_sentence[-1] = (pre_sentence, ner)
+            else:
+                segment_sentence.append(x)
+    
+    if state == 1:
+        return segment_sentence
+    return None
+    
